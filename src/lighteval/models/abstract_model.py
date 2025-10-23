3cc63152d7e478a4c9262f9b807621fe3132f546
Jeronymous <jeromelouradour@gmail.com>
2025-10-23 11:02:03 +0200

    Take HF_HOME env variable into account (if set)

--- src/lighteval/models/abstract_model.py
+++ src/lighteval/models/abstract_model.py
@@ -22,6 +22,7 @@
 
 import json
 import re
+import os
 from abc import ABC, abstractmethod
 from typing import Optional, Union
 
@@ -86,7 +87,7 @@ class ModelConfig(BaseModel, extra="forbid"):
 
     generation_parameters: GenerationParameters = GenerationParameters()
     system_prompt: str | None = None
-    cache_dir: str = "~/.cache/huggingface/lighteval"
+    cache_dir: str = os.path.join(os.environ.get("HF_HOME", "~/.cache/huggingface"), "lighteval")
 
     @classmethod
     def from_path(cls, path: str):
